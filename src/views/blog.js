cf54e32d0032f27621619310e953730093b9b0ca
tomaszpankowski <--get-all>
2022-05-23 10:45:13 +0200

    layout

--- src/views/blog.js
+++ src/views/blog.js
@@ -1,11 +1,19 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import BlogS1 from "../components/blog-s1";
+import BlogS2 from "../components/blog-s2";
+import BlogS3 from "../components/blog-s3";
 
-function Blog(){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            
-        </Container>
-    );
+class Blog extends Component{
+    render(){
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <BlogS1/>
+                <BlogS2/>
+                <BlogS3/>
+            </Container>    
+        );
+    }
 }
 
 export default Blog;
